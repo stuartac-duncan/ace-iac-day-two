138b54c622b6dc8dd0b23348391d7cdea1829715
stuartac-duncan <112838417+stuartac-duncan@users.noreply.github.com>
2022-09-07 17:41:49 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    # organization = "cyrustel"
+    organization = "cyrustel"
     workspaces {
       name = "ace-iac-day-two"
     }
